a784f74835926afb97aa12776293c488cc54ef99
megawac <megawac@gmail.com>
2014-04-25 13:24:08 -0400

    Fix signal hooks

--- run.py
+++ run.py
@@ -9,5 +9,7 @@ def on_pull(data):
     if data["action"] in ["opened", "reopened", "synchronize"]:
         jimaek.validate(data["number"])
 
+signal("pull_event").connect(on_pull)
+
 DEFAULT_PORT = int(os.environ.get("PORT", 9000))
 server.start(DEFAULT_PORT)
