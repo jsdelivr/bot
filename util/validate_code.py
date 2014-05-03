f2a6bf974a90d6999d60fa2e36f80174f55fa011
megawac <megawac@gmail.com>
2014-05-03 14:35:26 -0400

    Warn for unescaped unicode characters

--- util/validate_code.py
+++ util/validate_code.py
@@ -1,14 +1,14 @@
-import os, re
+import re
 
 # from https://github.com/hamilyon/status/blob/master/grin.py
-TEXTCHARS = ''.join(map(chr, [7,8,9,10,12,13,27] + range(0x20, 0x100)))
-ALLBYTES = ''.join(map(chr, range(256)))
+# TEXTCHARS = ''.join(map(chr, [7,8,9,10,12,13,27] + range(0x20, 0x100)))
+# ALLBYTES = ''.join(map(chr, range(256)))
 
-def is_binary_string(bytes):
-    bytes = bytes[:1024] #hack patch #3
-    if isinstance(bytes, unicode):
-        bytes = bytes.encode("ascii", errors="ignore")
-    return bool(bytes.translate(ALLBYTES, TEXTCHARS))
+# def is_binary_string(bytes):
+#     bytes = bytes[:1024] #hack patch #3
+#     if isinstance(bytes, unicode):
+#         bytes = bytes.encode("ascii", errors="ignore")
+#     return bool(bytes.translate(ALLBYTES, TEXTCHARS))
 
 comment_re = re.compile(r"(?:\/\*(?:[\s\S]*?)\*\/)|(?:([\s;])+\/\/(?:.*)$)", re.MULTILINE)
 
@@ -28,6 +28,8 @@ class CodeValidator():
         ".jpeg": True,
         ".gif": True,
         ".ico": True,
+        ".webp": True,
+        ".webm": True,
 
         #fonts
         ".otf": True,
@@ -47,10 +49,16 @@ class CodeValidator():
             if file["extension"] not in self.valid_extensions:
                 issues.append("*{extension}* (on *{name}*) seems odd to want to host?".format(**file))
                 continue
-            elif is_binary_string(file["contents"]) != self.valid_extensions.get(file["extension"]):
-                msg = "to be binary content" if self.valid_extensions.get(file["extension"]) else "to not be binary content"
-                issues.append("Expected *{name}* {0}".format(msg, **file))
+            elif file["contents"] is None:
+                if self.valid_extensions.get(file["extension"]) != True:
+                    issues.append("Expected *{name}* to be binary content".format(**file))
+                continue #binary file
+            elif self.valid_extensions.get(file["extension"]) != False:
+                issues.append("Expected *{name}* to be static content".format(**file))
                 continue
+
+            if file["contents"].strip() == "":
+                issues.append("Why is *{name}* an empty file?")
 
             if file["extension"] != ".js" and file["extension"] != ".css":
                 continue
@@ -69,8 +77,8 @@ class CodeValidator():
             #     issues.append("*{name}* ({version}) probably should start with a header detailing author and code source".format(**file))
 
             try:
-                file["contents"].encode("ascii")
-            except UnicodeEncodeError,e:
+                file["contents"].decode("ascii")
+            except UnicodeError,e:
                 issues.append("Unescaped unicode characters in *{name}* (at pos {0})".format(e.start, **file))
 
         return issues
