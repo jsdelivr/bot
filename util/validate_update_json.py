7d2171527224582a466d0695ae902a3dbc08f1ce
Graeme Yeates <yeatesgraeme@gmail.com>
2015-04-04 11:59:12 -0400

    Make basePath optional (jsdelivr/jsdelivr#4143 + others)

--- util/validate_update_json.py
+++ util/validate_update_json.py
@@ -13,8 +13,11 @@ class UpdateJSONValidator():
             if config["name"] != schema["project"]:
                 issues.append("Name mismatch for update.json in {project}".format(**schema))
 
-            if type(config["files"]) != dict or type(config["files"]["basePath"]) != unicode:
+            if "files" not in config or type(config["files"]) != dict:
                 issues.append("Missing files to include for update.json in {project}".format(**schema))
+
+            elif "basePath" in config["files"] and type(config["files"]["basePath"]) != unicode:
+                raise
 
             # Validate files exist... maybe later
         except Exception,e:
