4014d6efff57033439c5b8cece86930c0f5d80e1
Graeme Yeates <yeatesgraeme@gmail.com>
2015-05-04 18:31:29 -0400

    Make merge+squash script more robust

--- scripts/squash-and-merge.sh
+++ scripts/squash-and-merge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+set -e
 
 ## $1 is path
 ## $2 is remote
 ## $3 is branch
 ## $4 is the pr number
-## $5 is the number of commits
-## $6 is the new commit message
+## $5 is the new commit message
 
 # Requires git-extras (https://github.com/tj/git-extras)
 
@@ -16,20 +16,24 @@ git fetch $2
 git checkout $3
 git reset --hard $2/$3
 
-# Checkout and squash
+# Checkout and update
 git pr $4 $2
-git rebase $2
-git reset --soft HEAD~$(expr $5 - 1)
+git rebase $3
+
+# Squash
+COUNT="$(git missing $3 | wc -l)"
+git reset --soft HEAD~$(expr $COUNT>0?$COUNT-1:0)
 git add -A
-git commit --amend -m $6
+git commit --amend -m "$5"
 
 # merge into branch
 git merge-into $3
 
-BRANCH = git rev-parse --abbrev-ref HEAD
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 git checkout $3
 git branch -D $BRANCH
 
-git push $2 $3
+echo "still here"
+# git push $2 $3
 
-# echo ${PWD##*/}
+exit 200
--- util/merger.py
+++ util/merger.py
@@ -1,5 +1,4 @@
-from git import Repo
-import pystache, re
+import pystache, re, subprocess
 
 class GitMerger():
     """
@@ -30,50 +29,20 @@ class GitMerger():
         """
            Merge and squash the commits of a pull request if the requester is allowed
 
-           Merges in 2-3 commits depending if fast-forward is necessary
+           Merges in 1-2 commits depending if fast-forward is necessary
 
            @param number int pull number
-           @param requester str github login
-           @param comment str check if comment is requesting a merge 
         """
-        
         pr = self.get_pull(number)
-        repo = Repo(self.config["path_to_repo"])
 
-        repo.git.checkout("master")
-        repo.git.pull("origin", "master")
+        message = "%s\nCloses %d" % (pr.title, number)
 
-        try:
-            #for merging
-            remote = repo.create_remote("temp", "https://github.com/{0}/{1}.git".format(*pr.head.repo))
-            repo.git.fetch(remote, pr.head.ref)
-            #akin: git checkout -b temp-merge temp/<branch> --track
-            branch_name = "{0}/{1}".format(pr.user.login, pr.head.ref)
-            branch_location = "{0}/{1}".format(remote.name, pr.head.ref)
-            repo.git.checkout(branch_name, branch_location, b=True, track=True)
-            branch = repo.active_branch
+        output = subprocess.check_output(['./scripts/squash-and-merge.sh',
+            self.config["path_to_repo"],
+            self.config["repo_remote"],
+            self.config["repo_branch"],
+            str(number),
+            message
+        ])
 
-            #soft reset/squash it
-            reset_commits = max(0, pr.commits - self.get_number_commits(comment))
-            repo.git.reset("HEAD~{0}".format(reset_commits), soft=True)
-            repo.git.rebase("master")
-
-            with open("templates/merge-pull-request.tpl") as f:
-                commit_msg = pystache.render(f.read(), pr)
-                print commit_msg
-            # repo.index.commit(commit_msg)
-
-            # repo.git.checkout("master")
-            # repo.git.merge(branch)
-
-            # repo.remotes.origin.push("master")
-
-        except Exception,e:
-            issue = self.repo.issue(pr.number)
-            issue.create_comment("Could not automatically squash and merge the pull request: {0}".format(str(e)))
-        finally:
-            try:
-                #clean up
-                repo.delete_remote(remote)
-                repo.git.branch(branch, D=True) #delete branch
-            except: pass
+        print output
